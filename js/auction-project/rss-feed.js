70cbdd5a633e791a60bcd794f85b33ae5c4d1281
HarryMcKinney <harrypmckinney@gmail.com>
2015-12-29 15:55:44 -0500

    move rss feed out of script tags

---
+++ js/auction-project/rss-feed.js
@@ -0,0 +1,17 @@
+$(function(){
+    url = "http://rss.autoweek.com/?page=n-55998";
+    $.ajax({
+    type: "GET",
+    url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url),
+    dataType: 'json',
+    error: function(){
+        console.alert('autoweek rss feed not loaded');
+    },
+    success: function(xml){
+        values = xml.responseData.feed.entries;
+        console.info("Current Autoweek RSS feed: ",values);
+        console.info(values[0].title);
+        console.info(values[0].contentSnippet);
+    }
+  });
+});
