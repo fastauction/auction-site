6ada6af2f3e86a0bc25331c05af8669dbad3825a
HarryMcKinney <harrypmckinney@gmail.com>
2015-12-29 13:00:18 -0500

    move drop down js off of the page

---
+++ js/auction-project/infodrop.js
@@ -0,0 +1,95 @@
+$(document).ready(function auction1() {
+    $(".hx1-info").click(function() {
+        $(".more-info").slideDown("slow");
+    });
+});
+
+$(document).ready(function auction2() {
+    $(".close-window").click(function() {
+        $(".more-info").hide("slow");
+    });
+});
+
+$(document).ready(function auction3() {
+    $(".2-hx1-info").click(function() {
+        $(".2-more-info").slideDown("slow");
+    });
+});
+
+$(document).ready(function auction4() {
+    $(".2-close-window").click(function() {
+        $(".2-more-info").hide("slow");
+    });
+});
+
+$(document).ready(function auction5() {
+    $(".3-hx1-info").click(function() {
+        $(".3-more-info").slideDown("slow");
+    });
+});
+
+$(document).ready(function auction6() {
+    $(".3-close-window").click(function() {
+        $(".3-more-info").hide("slow");
+    });
+});
+
+$(document).ready(function auction7() {
+    $(".4-hx1-info").click(function() {
+        $(".4-more-info").slideDown("slow");
+    });
+});
+
+$(document).ready(function auction8() {
+    $(".4-close-window").click(function() {
+        $(".4-more-info").hide("slow");
+    });
+});
+
+$(document).ready(function auction9() {
+    $(".5-hx1-info").click(function() {
+        $(".5-more-info").slideDown("slow");
+    });
+});
+
+$(document).ready(function auction10() {
+    $(".5-close-window").click(function() {
+        $(".5-more-info").hide("slow");
+    });
+});
+
+$(document).ready(function auction11() {
+    $(".6-hx1-info").click(function() {
+        $(".6-more-info").slideDown("slow");
+    });
+});
+
+$(document).ready(function auction12() {
+    $(".6-close-window").click(function() {
+        $(".6-more-info").hide("slow");
+    });
+});
+
+$(document).ready(function auction13() {
+    $(".7-hx1-info").click(function() {
+        $(".7-more-info").slideDown("slow");
+    });
+});
+
+$(document).ready(function auction14() {
+    $(".7-close-window").click(function() {
+        $(".7-more-info").hide("slow");
+    });
+});
+
+$(document).ready(function auction15() {
+    $(".8-hx1-info").click(function() {
+        $(".8-more-info").slideDown("slow");
+    });
+});
+
+$(document).ready(function auction16() {
+    $(".8-close-window").click(function() {
+        $(".8-more-info").hide("slow");
+    });
+});
