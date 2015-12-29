17eed7b5c0b4bdb9f4b00df0fb3790f21a6537b4
HarryMcKinney <harrypmckinney@gmail.com>
2015-12-29 12:54:40 -0500

    remove comma separator from page

---
+++ js/auction-project/pricecomma.js
@@ -0,0 +1,7 @@
+//function to add commas to price
+function commaSeparateNumber(val) {
+    while (/(\d+)(\d{3})/.test(val.toString())) {
+        val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
+    }
+    return val;
+}
