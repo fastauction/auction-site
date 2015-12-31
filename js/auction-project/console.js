f742873671ba37b28286a4c54ff40e4d267715a4
HarryMcKinney <harrypmckinney@gmail.com>
2015-12-31 14:22:02 -0500

    add console alert to pre prod

--- js/auction-project/console.js
+++ js/auction-project/console.js
@@ -1,3 +1,3 @@
 console.info("welcome to my Auction Trader live auction website :)");
 console.info("my Auction Trader is an Aucion Overdrive company, All Rights Reserved");
-//alert("This is a BETA version of my Auction Trader, please do not bid yet!");
+alert("You are viewing a PRE-PRODUCTION release of My Auction Trader, please do not place any bids!");
