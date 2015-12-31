1c2ca94182341c07ea0352568d2bd8d4b9b82e00
HarryMcKinney <harrypmckinney@gmail.com>
2015-12-31 14:24:51 -0500

    test colors in console

--- js/auction-project/console.js
+++ js/auction-project/console.js
@@ -1,3 +1,4 @@
 console.info("welcome to my Auction Trader live auction website :)");
 console.info("my Auction Trader is an Aucion Overdrive company, All Rights Reserved");
 alert("You are viewing a PRE-PRODUCTION release of My Auction Trader, please do not place any bids!");
+console.log("%cBlue! %cGreen", "color: blue; font-size:15px;", "color: green; font-size:12px;", "Test!");
