36335123ab89d2f4a3b17c9f3e26920acf10a284
HarryMcKinney <harrypmckinney@gmail.com>
2015-12-31 14:27:12 -0500

    add message to styled console logs

--- js/auction-project/console.js
+++ js/auction-project/console.js
@@ -1,4 +1,4 @@
 console.info("welcome to my Auction Trader live auction website :)");
 console.info("my Auction Trader is an Aucion Overdrive company, All Rights Reserved");
 alert("You are viewing a PRE-PRODUCTION release of My Auction Trader, please do not place any bids!");
-console.log("%cBlue! %cGreen", "color: blue; font-size:15px;", "color: green; font-size:12px;", "Test!");
+console.log("%cMy Auction Trader! %cYou are viewing a PRE PRE-PRODUCTION release of the site", "color: blue; font-size:15px;", "color: green; font-size:12px;");
