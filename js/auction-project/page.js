f8d21eeaba8f3f23c28317d335b6f29d3246481f
HarryMcKinney <harrypmckinney@gmail.com>
2016-01-12 16:23:02 -0500

    remove the href link

--- js/auction-project/page.js
+++ js/auction-project/page.js
@@ -1,3 +1,5 @@
 var xxx = window.location.href;
   console.info("%c" + xxx,"background-color:#fbc97f");
+var yyy = window.location;
+  console.info("%c" + yyy,"background-color:#fbc97f");
   //get page url
