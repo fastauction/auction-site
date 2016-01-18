2058d088af8f2ab7e84217924b67ff96d375d209
HarryMcKinney <harrypmckinney@gmail.com>
2016-01-18 13:34:19 -0500

    add curl method

--- php/data.php
+++ php/data.php
@@ -1,1 +1,11 @@
-<?php echo ("auction data"); ?>
+<?php echo ("auction data");
+$url = "http://www.beta-myauctiontrader.com/data/data.json";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$url);
+$data=curl_exec($ch);
+curl_close($ch);
+$json = json_decode($data, true);
+echo $json;
+?>
