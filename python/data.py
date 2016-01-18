b219374ee42900faff314fd9af47d780d8a05210
HarryMcKinney <harrypmckinney@gmail.com>
2016-01-18 13:28:07 -0500

    changes to python data loader

--- python/data.py
+++ python/data.py
@@ -1,4 +1,6 @@
 import json
 import urllib2
 
-auctioninventory = json.load(urllib2.urlopen('././data/data.json'))
+auctioninventory = json.load(urllib2.urlopen('http://www.beta-myauctiontrader.com/data/data.json'))
+
+print auctioninventory
